cdfc06936aea2124fe3981bc0ed555560760e7fe
Agustin Rosso <AgustinRosso_1@hotmail.com>
2026-01-18 17:26:27 +0100

    Se agregan tests

---
+++ Tests/StateMachineTests.cs
@@ -0,0 +1,211 @@
+using NUnit.Framework;
+using Rossoforge.Utils.StateMachine;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Rossoforge.Utils.Tests
+{
+    public class StateMachineTests
+    {
+        private TestStateMachine machine;
+        private FakeState stateA;
+        private FakeState stateB;
+
+        [SetUp]
+        public void Setup()
+        {
+            machine = new TestStateMachine();
+            stateA = new FakeStateA();
+            stateB = new FakeStateB();
+        }
+
+        [Test]
+        public void StartMachine_ShouldSetInitialState()
+        {
+            machine.StartMachine(stateA);
+
+            Assert.AreEqual(stateA, machine.CurrentState);
+            Assert.IsTrue(stateA.EnterCalled);
+        }
+
+        [Test]
+        public void StartMachine_WithNull_ShouldThrow()
+        {
+            Assert.Throws<ArgumentNullException>(() => machine.StartMachine(null));
+        }
+
+        [Test]
+        public async Task TransitionTo_ShouldChangeState()
+        {
+            machine.StartMachine(stateA);
+
+            var result = await machine.TransitionTo(stateB);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(stateB, machine.CurrentState);
+            Assert.AreEqual(stateA, machine.PreviousState);
+
+            Assert.IsTrue(stateA.ExitCalled);
+            Assert.IsTrue(stateB.EnterCalled);
+        }
+
+        [Test]
+        public void TransitionTo_SameState_ShouldReturnFalse()
+        {
+            machine.StartMachine(stateA);
+
+            var result = machine.TransitionTo(stateA).GetAwaiter().GetResult();
+
+            Assert.IsFalse(result);
+            Assert.AreEqual(stateA, machine.CurrentState);
+        }
+
+        [Test]
+        public void TransitionTo_BeforeStart_ShouldReturnFalse()
+        {
+            var result = machine.TransitionTo(stateA).GetAwaiter().GetResult();
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task TransitionToPreviousState_ShouldReturnToPrevious()
+        {
+            machine.StartMachine(stateA);
+
+            await machine.TransitionTo(stateB);
+            var result = await machine.TransitionToPreviousState();
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(stateA, machine.CurrentState);
+        }
+
+        [Test]
+        public void TransitionToPreviousState_WithNoPrevious_ShouldReturnFalse()
+        {
+            machine.StartMachine(stateA);
+
+            var result = machine.TransitionToPreviousState().GetAwaiter().GetResult();
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Update_ShouldCallCurrentStateUpdate()
+        {
+            machine.StartMachine(stateA);
+
+            machine.Update();
+
+            Assert.IsTrue(stateA.UpdateCalled);
+        }
+
+        [Test]
+        public void Update_ShouldNotRunDuringTransition()
+        {
+            machine.StartMachine(stateA);
+
+            // Simulamos estado de transición
+            typeof(StateMachine<FakeState>)
+                .GetProperty("IsTransitionInProgress")
+                .SetValue(machine, true);
+
+            machine.Update();
+
+            Assert.IsFalse(stateA.UpdateCalled);
+        }
+
+        [Test]
+        public void StartMachine_Twice_ShouldNotOverrideCurrentState()
+        {
+            machine.StartMachine(stateA);
+            machine.StartMachine(stateB);
+
+            Assert.AreEqual(stateA, machine.CurrentState);
+        }
+
+        [Test]
+        public void GetPreviousStateType_ShouldReturnCorrectType()
+        {
+            machine.StartMachine(stateA);
+            machine.TransitionTo(stateB).GetAwaiter().GetResult();
+
+            var type = machine.GetPreviousStateType();
+
+            Assert.AreEqual(typeof(FakeStateA), type);
+        }
+
+        [Test]
+        public async Task TransitionTo_ShouldCallExitThenEnter()
+        {
+            machine.StartMachine(stateA);
+
+            await machine.TransitionTo(stateB);
+
+            CollectionAssert.AreEqual(
+                new List<string> { "Enter", "Exit" },
+                stateA.CallOrder
+            );
+
+            CollectionAssert.AreEqual(
+                new List<string> { "Enter" },
+                stateB.CallOrder
+            );
+        }
+
+        [Test]
+        public async Task TransitionTo_NullState_ShouldReturnFalse()
+        {
+            machine.StartMachine(stateA);
+
+            var result = await machine.TransitionTo(null);
+
+            Assert.IsFalse(result);
+        }
+    }
+
+    public class TestStateMachine : StateMachine<FakeState>
+    {
+    }
+
+    public class FakeState : IState
+    {
+        public bool EnterCalled { get; private set; }
+        public bool ExitCalled { get; private set; }
+        public bool UpdateCalled { get; private set; }
+
+        public List<string> CallOrder = new();
+
+        public void Enter()
+        {
+            EnterCalled = true;
+            CallOrder.Add("Enter");
+        }
+
+        public void Exit()
+        {
+            ExitCalled = true;
+            CallOrder.Add("Exit");
+        }
+
+        public void Update()
+        {
+            UpdateCalled = true;
+            CallOrder.Add("Update");
+        }
+
+        public void Reset()
+        {
+            EnterCalled = ExitCalled = UpdateCalled = false;
+            CallOrder.Clear();
+        }
+    }
+    public class FakeStateA : FakeState
+    {
+    }
+
+    public class FakeStateB : FakeState
+    {
+    }
+}
