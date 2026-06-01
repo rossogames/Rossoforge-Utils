c34c29efa5d25fcdd9444194c3cef9fb76406df4
Agustin Rosso <agustin.rosso.code@gmail.com>
2026-06-01 11:14:03 +0200

    cambios en nomenclaturas

--- Tests/PlayerPrefsStorageTests.cs
+++ Tests/PlayerPrefsStorageTests.cs
@@ -4,7 +4,7 @@ using Rossoforge.Utils.IO;
 
 namespace Rossoforge.Utils.Tests
 {
-    public class PlayerPrefsStoreTests
+    public class PlayerPrefsStorageTests
     {
         private const string IntKey = "test_int";
         private const string FloatKey = "test_float";
@@ -36,9 +36,9 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void SaveInt_LoadInt_ReturnsSavedValue()
         {
-            PlayerPrefsStore.SaveInt(IntKey, 42);
+            PlayerPrefsStorage.SaveInt(IntKey, 42);
 
-            int value = PlayerPrefsStore.LoadInt(IntKey);
+            int value = PlayerPrefsStorage.LoadInt(IntKey);
 
             Assert.AreEqual(42, value);
         }
@@ -46,7 +46,7 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void LoadInt_ReturnsDefault_WhenKeyDoesNotExist()
         {
-            int value = PlayerPrefsStore.LoadInt(IntKey, 99);
+            int value = PlayerPrefsStorage.LoadInt(IntKey, 99);
 
             Assert.AreEqual(99, value);
         }
@@ -56,9 +56,9 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void SaveFloat_LoadFloat_ReturnsSavedValue()
         {
-            PlayerPrefsStore.SaveFloat(FloatKey, 3.14f);
+            PlayerPrefsStorage.SaveFloat(FloatKey, 3.14f);
 
-            float value = PlayerPrefsStore.LoadFloat(FloatKey);
+            float value = PlayerPrefsStorage.LoadFloat(FloatKey);
 
             Assert.AreEqual(3.14f, value);
         }
@@ -66,7 +66,7 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void LoadFloat_ReturnsDefault_WhenKeyDoesNotExist()
         {
-            float value = PlayerPrefsStore.LoadFloat(FloatKey, 1.5f);
+            float value = PlayerPrefsStorage.LoadFloat(FloatKey, 1.5f);
 
             Assert.AreEqual(1.5f, value);
         }
@@ -76,9 +76,9 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void SaveString_LoadString_ReturnsSavedValue()
         {
-            PlayerPrefsStore.SaveString(StringKey, "hello");
+            PlayerPrefsStorage.SaveString(StringKey, "hello");
 
-            string value = PlayerPrefsStore.LoadString(StringKey);
+            string value = PlayerPrefsStorage.LoadString(StringKey);
 
             Assert.AreEqual("hello", value);
         }
@@ -86,7 +86,7 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void LoadString_ReturnsDefault_WhenKeyDoesNotExist()
         {
-            string value = PlayerPrefsStore.LoadString(StringKey, "default");
+            string value = PlayerPrefsStorage.LoadString(StringKey, "default");
 
             Assert.AreEqual("default", value);
         }
@@ -96,9 +96,9 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void SaveBool_LoadBool_ReturnsTrue()
         {
-            PlayerPrefsStore.SaveBool(BoolKey, true);
+            PlayerPrefsStorage.SaveBool(BoolKey, true);
 
-            bool value = PlayerPrefsStore.LoadBool(BoolKey);
+            bool value = PlayerPrefsStorage.LoadBool(BoolKey);
 
             Assert.IsTrue(value);
         }
@@ -106,9 +106,9 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void SaveBool_LoadBool_ReturnsFalse()
         {
-            PlayerPrefsStore.SaveBool(BoolKey, false);
+            PlayerPrefsStorage.SaveBool(BoolKey, false);
 
-            bool value = PlayerPrefsStore.LoadBool(BoolKey);
+            bool value = PlayerPrefsStorage.LoadBool(BoolKey);
 
             Assert.IsFalse(value);
         }
@@ -116,7 +116,7 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void LoadBool_ReturnsDefault_WhenKeyDoesNotExist()
         {
-            bool value = PlayerPrefsStore.LoadBool(BoolKey, true);
+            bool value = PlayerPrefsStorage.LoadBool(BoolKey, true);
 
             Assert.IsTrue(value);
         }
@@ -126,9 +126,9 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void SaveEnum_LoadEnum_ReturnsSavedValue()
         {
-            PlayerPrefsStore.SaveEnum(EnumKey, TestEnum.C);
+            PlayerPrefsStorage.SaveEnum(EnumKey, TestEnum.C);
 
-            TestEnum value = PlayerPrefsStore.LoadEnum(EnumKey, TestEnum.A);
+            TestEnum value = PlayerPrefsStorage.LoadEnum(EnumKey, TestEnum.A);
 
             Assert.AreEqual(TestEnum.C, value);
         }
@@ -136,7 +136,7 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void LoadEnum_ReturnsDefault_WhenKeyDoesNotExist()
         {
-            TestEnum value = PlayerPrefsStore.LoadEnum(EnumKey, TestEnum.B);
+            TestEnum value = PlayerPrefsStorage.LoadEnum(EnumKey, TestEnum.B);
 
             Assert.AreEqual(TestEnum.B, value);
         }
@@ -146,9 +146,9 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void HasKey_ReturnsTrue_WhenKeyExists()
         {
-            PlayerPrefsStore.SaveInt(IntKey, 10);
+            PlayerPrefsStorage.SaveInt(IntKey, 10);
 
-            bool exists = PlayerPrefsStore.HasKey(IntKey);
+            bool exists = PlayerPrefsStorage.HasKey(IntKey);
 
             Assert.IsTrue(exists);
         }
@@ -156,7 +156,7 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void HasKey_ReturnsFalse_WhenKeyDoesNotExist()
         {
-            bool exists = PlayerPrefsStore.HasKey(IntKey);
+            bool exists = PlayerPrefsStorage.HasKey(IntKey);
 
             Assert.IsFalse(exists);
         }
@@ -164,11 +164,11 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void DeleteKey_RemovesKey()
         {
-            PlayerPrefsStore.SaveInt(IntKey, 5);
+            PlayerPrefsStorage.SaveInt(IntKey, 5);
 
-            PlayerPrefsStore.DeleteKey(IntKey);
+            PlayerPrefsStorage.DeleteKey(IntKey);
 
-            bool exists = PlayerPrefsStore.HasKey(IntKey);
+            bool exists = PlayerPrefsStorage.HasKey(IntKey);
 
             Assert.IsFalse(exists);
         }
@@ -176,21 +176,21 @@ namespace Rossoforge.Utils.Tests
         [Test]
         public void DeleteAll_RemovesAllKeys()
         {
-            PlayerPrefsStore.SaveInt(IntKey, 1);
-            PlayerPrefsStore.SaveFloat(FloatKey, 2f);
+            PlayerPrefsStorage.SaveInt(IntKey, 1);
+            PlayerPrefsStorage.SaveFloat(FloatKey, 2f);
 
-            PlayerPrefsStore.DeleteAll();
+            PlayerPrefsStorage.DeleteAll();
 
-            Assert.IsFalse(PlayerPrefsStore.HasKey(IntKey));
-            Assert.IsFalse(PlayerPrefsStore.HasKey(FloatKey));
+            Assert.IsFalse(PlayerPrefsStorage.HasKey(IntKey));
+            Assert.IsFalse(PlayerPrefsStorage.HasKey(FloatKey));
         }
 
         [Test]
         public void Save_CanBeCalledWithoutError()
         {
-            PlayerPrefsStore.SaveInt("dummy", 123);
+            PlayerPrefsStorage.SaveInt("dummy", 123);
 
-            Assert.DoesNotThrow(() => PlayerPrefsStore.Save());
+            Assert.DoesNotThrow(() => PlayerPrefsStorage.Save());
         }
     }
 }
